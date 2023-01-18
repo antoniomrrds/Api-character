baebfc8db050b82f2070b3351a0121a9c335ee52
antoniomrrds <arqueiroverd@gmail.com>
2023-01-17 21:24:52 -0300

    adding swagger routes

--- src/app.js
+++ src/app.js
@@ -26,9 +26,9 @@ const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'Library API',
+      title: 'Phrases API',
       version: '1.0.0',
-      description: 'A simple Express Library API',
+      description: 'A simple API',
     },
     servers: [
       {
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 const router = require('express').Router();
 const PhraseController = require('../controllers/PhraseController');
 const HomeController = require('../controllers/HomeController');
@@ -20,13 +21,14 @@ const HomeController = require('../controllers/HomeController');
  *           description: The phrase
  *         author:
  *           type: string
- *           description: The phrase author
+ *           description: The author of the phrase
  *       example:
  *         id: 1
- *         phrase: The New Turing Omnibus
- *         author: Alexander K. Dewdney
+ *         phrase: 'Covarde não é aquele que evita um combate, covarde é aquele que mesmo sabendo que é superior luta e fere o mais fraco.'
+ *         author: Bruce Lee
  */
 
+router.get('/', HomeController.index);
 /**
  * @swagger
  * tags:
@@ -34,28 +36,82 @@ const HomeController = require('../controllers/HomeController');
  *   description: The Phrase managing API
  */
 
+router.get('/phrase', PhraseController.index);
+
 /**
  * @swagger
- * /books:
+ * /phrase:
  *   get:
- *     summary: Returns the list of all the books
+ *     summary: Returns the list of all the phrase
  *     tags: [Phrase]
  *     responses:
  *       200:
- *         description: The list of the books
+ *         description: The list of the phrase
  *         content:
  *           application/json:
  *             schema:
  *               type: array
  *               items:
  *                 $ref: '#/components/schemas/Phrase'
+ *       500:
+ *        description: Some error happened
  */
 
-router.get('/', HomeController.index);
-
-router.get('/phrase', PhraseController.index);
 router.get('/phrase/:id', PhraseController.show);
+
+/**
+ * @swagger
+ * /phrase/{id}:
+ *   get:
+ *     summary : Get The Phrase by id
+ *     tags: [Phrase]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: number
+ *         required: true
+ *         description: The phrase id
+ *     responses:
+ *       200:
+ *         description: The Phrase description by id
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Phrase'
+ *       400:
+ *         description: Malformed request syntax
+ *       404:
+ *         description: The phrase was not found
+ *       500:
+ *         description: Some error happened
+ */
+
 router.post('/phrase', PhraseController.create);
+
+/**
+ * @swagger
+ * /phrase:
+ *   post:
+ *      summary : Create a new phrase
+ *      tags: [Phrase]
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schema/Phrase'
+ *      responses:
+ *        201:
+ *          description: The phrase was successfully created
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/Phrase'
+ *        500:
+ *         description: Some error happened
+ */
+
 router.put('/phrase/:id', PhraseController.update);
 router.delete('/phrase/:id', PhraseController.delete);
 
